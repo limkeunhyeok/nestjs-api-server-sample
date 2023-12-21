58c6a73d46161bc501fccd5fb284342a38165eb7
limkeunhyeok <gorloom6425@naver.com>
2023-12-21 17:35:56 +0900

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+// https://pm2.keymetrics.io/
+module.exports = {
+  apps: [
+    {
+      name: 'server-dev',
+      script: 'npm',
+      args: 'run start:dev',
+      exec_mode: 'fork',
+      instance_var: 'INSTANCE_ID',
+      instances: 1,
+      autorestart: true,
+      env: {},
+    },
+  ],
+};

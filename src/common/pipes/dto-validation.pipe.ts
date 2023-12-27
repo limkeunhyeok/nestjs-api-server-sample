65ec8f29a2ddd4137df8ae337b9c50dd5e665030
limkeunhyeok <gorloom6425@naver.com>
2023-12-27 23:00:05 +0900

    feat: add dto validation pipe

---
+++ src/common/pipes/dto-validation.pipe.ts
@@ -0,0 +1,33 @@
+import { classToClass } from '@nestjs/class-transformer';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class DtoValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToInstance(metatype, value);
+    const converted = classToClass(object);
+
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new BadRequestException('Request data is invalid.', String(errors));
+    }
+
+    return converted;
+  }
+
+  private toValidate(metatype): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { DtoValidationPipe } from './common/pipes/dto-validation.pipe';
 import { serverConfig } from './config';
 import { LogCategory, logger } from './libs/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new DtoValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(serverConfig.port, () => {

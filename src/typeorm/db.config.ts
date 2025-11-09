1483f39365f551032bcfce9918783bf684019402
limkeunhyeok <gorloom6425@naver.com>
2025-11-09 14:38:12 +0900

    chore: update node version and migrate to pnpm

--- src/typeorm/db.config.ts
+++ src/typeorm/db.config.ts
@@ -19,8 +19,8 @@ export const getDbConfig = (entities: any): TypeOrmModuleOptions => {
         ? false
         : true,
     entities,
-    migrationsRun: true,
-    migrations: [__dirname + '/**/migrations/*{.ts,.js}'],
-    metadataTableName: 'migrations',
+    // migrationsRun: true,
+    // migrations: [__dirname + '/**/migrations/*{.ts,.js}'],
+    // metadataTableName: 'migrations',
   };
 };

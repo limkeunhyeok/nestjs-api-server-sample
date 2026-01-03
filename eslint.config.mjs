cbd225fc4c8f0404d3537e3d1d4ae8f0dc8722f8
limkeunhyeok <gorloom6425@naver.com>
2026-01-03 08:40:28 +0000

    fix: ignore test files in esline config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,13 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'ecosystem.config.js', 'test/'],
+    ignores: [
+      'eslint.config.mjs',
+      'ecosystem.config.js',
+      'test/',
+      '**/*.spec.ts',
+      '**/*.e2e-spec.ts',
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
